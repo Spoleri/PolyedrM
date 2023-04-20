6b3ab467e7fbee9158d69cdd82c522453cd8ce9a
Spoleri <130367574+Spoleri@users.noreply.github.com>
2023-04-20 12:05:14 +0300

    Add files via upload

---
+++ run_shadow.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env -S python3 -B
+
+from time import time
+from common.tk_drawer import TkDrawer
+from shadow.polyedr import Polyedr
+
+
+tk = TkDrawer()
+try:
+    for name in ["ccc", "box", "cube", "king"]:
+        print("=============================================================")
+        print(f"Начало работы с полиэдром '{name}'")
+        start_time = time()
+        a = Polyedr(f"data/{name}.geom")
+        print(f"Сумма длин ребер {a.sum_edgess()}")
+        a.draw(tk)
+        delta_time = time() - start_time
+        print(f"Изображение полиэдра '{name}' заняло {delta_time} сек.")
+        input("Hit 'Return' to continue -> ")
+except(EOFError, KeyboardInterrupt):
+    print("\nStop")
+    tk.close()
